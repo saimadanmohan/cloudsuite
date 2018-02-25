3d09bf020c4d9bfb29c8ed0343f05cff5b1e7af5
saimadanmohan <ubuntu@webservertest.cloud.compas.cs.stonybrook.edu>
2018-02-25 17:16:42 +0000

    implemented the real videos generation in place of text file based video payloads for media streaming

--- benchmarks/media-streaming/dataset/files/filegen/generate_video_files_and_logs.sh
+++ benchmarks/media-streaming/dataset/files/filegen/generate_video_files_and_logs.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 VIDEOS_DIR=$1
+VIDEO_SET=$2
 mkdir -p "$VIDEOS_DIR/logs"
 
 for paramfile in params/*; do
+  mkdir /workspace/textpaths
   cp "$paramfile" filegen_param.conf
   ./make_zipf
-  ./gen_fileset "$VIDEOS_DIR/full-$(basename $paramfile)-" video_files.txt
+  python3 ./video_gen.py -p filegen_param.conf -w /workspace -v video_files.txt -s "$VIDEO_SET" -o "$VIDEOS_DIR/"
   cp cl* "$VIDEOS_DIR/logs"
+  rm -rf /workspace/*
 done
